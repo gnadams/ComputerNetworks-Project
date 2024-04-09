43142ca69a54d8340284d791db2fab76479a66c4
Greg Adams <mowgli.gregory1@gmail.com>
2024-04-09 18:25:09 -0400

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel): # class for storing bandwidth
+    error: bool
+    upload: float
+    download: float
+    ping: float
+
+
+app = FastAPI()
+
+bandwidth = Item
+bandwidth.error = True
+bandwidth.upload = 0
+bandwidth.download = 0
+bandwidth.ping = 0
+
+
+@app.get("/")
+async def root():
+    return {"message":"Hello World"}
+
+@app.get("/bandwidth")
+async def receive_bandwidth():
+    if bandwidth.error != True:
+        return {"upload": bandwidth.upload, "download": bandwidth.download, "ping":bandwidth.ping}
+    else:
+        return {"Error": "Bandwidth has not been uploaded yet"}
+    #speed = get_network_speed()
+    #return speed
+    #if speed["error"] == "False":
+    #    return speed
+    #else:
+    #    return {"error": "True"}
+
+
+
+@app.post("/upload")
+async def upload_bandwidth(item: Item):
+    item_dict = item.dict()
+    if item_dict["error"] != True:
+        bandwidth.error = False
+        bandwidth.upload = item_dict["upload"]
+        bandwidth.download = item_dict["download"]
+        bandwidth.ping = item_dict["ping"]
+    return item_dict
---
+++ network.py
@@ -0,0 +1,25 @@
+import speedtest   
+
+
+
+def get_network_speed():
+    st = speedtest.Speedtest() 
+    error = False
+    down_speed=0
+    up_speed=0
+    try:
+        
+        down_speed = st.download()
+        up_speed = st.upload()
+        ping = st.results.ping
+        
+    except:
+        error = True
+
+    return {"error": error,"upload" : up_speed, "download": down_speed, "ping":ping}
+
+joe = get_network_speed()
+
+print(joe)
+
+    
