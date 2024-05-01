2af0daeb00fb6bac516e3b21b5e028c154ff3cb6
Greg Adams <mowgli.gregory1@gmail.com>
2024-05-01 02:19:29 -0400

    update db

--- localHostFiles/test.py
+++ localHostFiles/test.py
@@ -5,13 +5,13 @@ from datetime import *
 GLOBAL_URL = "https://computernetworks-project.onrender.com/upload/"
 LOCAL_URL = "http://127.0.0.1:8000/upload/" 
 
-time_now = datetime.now(timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
+time_now = (datetime.now(timezone.utc)-timedelta(hours=4)).strftime("%m/%d/%Y, %H:%M:%S")
 body = {
   "error": False,
-  "upload": 400.51, # speed of upload in MB
-  "download": 400.45, # speed of download in MB
+  "upload": 19, # speed of upload in MB
+  "download": 394, # speed of download in MB
   "date": time_now, # time in string, not really used but needed for schema
-  "ping" : 0, # Response time from server
+  "ping" : 59, # Response time from server
   "id": 0, # ID is not important, only required for schema
 }
 json_data = json.dumps(body)
